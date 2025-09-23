fe3d7802a8505a097015e9428f6e8983ae872530
hosnirabi <1hosnirabi@gmail.com>
2025-09-23 23:59:15 +0300

    calculator
    EZ

---
+++ clc.py
@@ -0,0 +1,13 @@
+n1=float(input("but your first number:"))
+n2=float(input("but your second number:"))
+s=input("choose one (+,-,*,/):")
+if s == "+":
+    print(n1+n2)
+elif s == "-":
+    print(n1-n2)
+elif s== "*":
+    print(n1*n2)
+elif s== "/":
+    print(n1/n2)
+else:
+ print("eror")
