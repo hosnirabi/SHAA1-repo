d79ce8fbcea58f3d2d53c8fe944210c33b2efff5
Hosnirabi <1hosnirabi@gmail.com>
2025-10-05 21:22:46 +0300

    homework1
    homework1

---
+++ f3.py
@@ -0,0 +1,3 @@
+a= int(input("chose a number:"))
+for i in range(1 ,a+1):
+ print(i)
---
+++ f6.py
@@ -0,0 +1,8 @@
+a=int(input("chose a number:"))
+t=0
+
+for i in range(1,a+1):
+    t +=i
+
+print("total=",t)
+ 
